5f74602b44a49a7497a80c9b3f55200e43f6f375
NMProgrammer <nathanwijayasekara@gmail.com>
2024-01-03 15:51:25 +0530

    Add validates

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, Matches, MinLength } from 'class-validator';
 
 export class RegisterUserDto {
   @ApiProperty()
@@ -12,6 +12,10 @@ export class RegisterUserDto {
 
   @ApiProperty()
   @IsNotEmpty()
+  @Matches(/^(?=.*[A-Z])/, {message: "Use Upper Case Letters as well"})
+  @Matches(/^(?=.*[a-z])/, {message: "Use Lower Case Letter as well"})
+  @Matches(/^(?=.*\d)/, {message: "Atleast one number should be used"})
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
 
