71afad2cec9374aaac9b6e06e7423b29d2f8d1b1
NMProgrammer <nathanwijayasekara@gmail.com>
2024-01-04 14:12:40 +0530

    Added Update Post Feature

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Request,
   Get,
+  Put,
+  Param,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PostsService } from './posts.service';
@@ -22,6 +24,14 @@ export class PostsController {
     return await this.postsService.getPosts();
   }
 
+  @ApiOperation({ summary: 'Give post according to id' })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard())
+  @Get(':id')
+  async viewPost(@Param('id') postId) {
+    return await this.postsService.getPostById(postId);
+  }
+
   @ApiOperation({ summary: 'Gives posts of a specific user' })
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
@@ -35,13 +45,27 @@ export class PostsController {
     description: 'Post Details',
     type: CreatePostDto,
   })
-  
   @ApiBearerAuth()
   @UseGuards(AuthGuard())
-  @Post('create')
+  @Post('')
   async createPost(@Body() data: CreatePostDto, @Request() req) {
     const new_post = await this.postsService.createPost({
       userId: req.user.id,
+      title: data.title,
+      body: data.body,
+    });
+    return new_post;
+  }
+
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard())
+  @Put(':id')
+  async updatePost(
+    @Param('id') postId,
+    @Body() data: CreatePostDto,
+    @Request() req,
+  ) {
+    const new_post = await this.postsService.updatePost(postId, {
       title: data.title,
       body: data.body,
     });
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -3,28 +3,43 @@ import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class PostsService {
-    constructor(private prisma: PrismaService) {}
-    async createPost(data: {userId: string, title: string, body: string}) {
-        const new_post = this.prisma.post.create({
-            data: {
-                user: {
-                    connect: {
-                        id: data.userId
-                    }
-                },
-                title: data.title,
-                body: data.body
-            }
-        })
+  constructor(private prisma: PrismaService) {}
+  async createPost(data: { userId: string; title: string; body: string }) {
+    const newPost = this.prisma.post.create({
+      data: {
+        user: {
+          connect: {
+            id: data.userId,
+          },
+        },
+        title: data.title,
+        body: data.body,
+      },
+    });
 
-        return new_post
-    }
+    return newPost;
+  }
 
-    async getUserPosts(userId) {
-        return await this.prisma.post.findMany({where: {userId: userId}})
-    }
+  async updatePost(postId: number, data: { title: string; body: string }) {
+    const updatedPost = this.prisma.post.update({
+      where: {
+        id: postId,
+      },
+      data,
+    });
 
-    async getPosts() {
-        return await this.prisma.post.findMany()
-    }
+    return updatedPost;
+  }
+
+  async getUserPosts(userId) {
+    return await this.prisma.post.findMany({ where: { userId: userId } });
+  }
+
+  async getPosts() {
+    return await this.prisma.post.findMany();
+  }
+
+  async getPostById(postId: number) {
+    return await this.prisma.post.findUnique({ where: { id: postId } });
+  }
 }
